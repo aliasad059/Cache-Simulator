eee0db51babeef4c82d0221a721aee786adffd8a
Amir01 <iran.volleyball79@gmail.com>
2021-04-06 23:23:33 +0430

    feat: add block_num and word list in cache representation

--- python/main.py
+++ python/main.py
@@ -5,8 +5,8 @@ from sys import maxsize
 import termtables
 
 # these sizes are in Byte
-BLOCK_SIZE = 1
-CACHE_SIZE = 4
+BLOCK_SIZE = 2
+CACHE_SIZE = 8
 MEMORY_SIZE = 1
 
 requests = list()
@@ -16,17 +16,31 @@ if not hexa:
     requests = list(map(int, inputs))
 else: 
     requests = list(map(lambda x:int(x, base=16), inputs))
-# BLOCK_SIZE = int(input("Size of Block: "))
-# CACHE_SIZE = int(input("Size of Cache: "))
-# MEMORY_SIZE = int(input("Size of Memory: "))
+BLOCK_SIZE = int(input("Size of Block: "))
+CACHE_SIZE = int(input("Size of Cache: "))
+MEMORY_SIZE = int(input("Size of Memory: "))
 
 
 def draw_table(cache: list(deque()), block_num: int, k:int) -> None:
+    """
+    Draw cache as a table in console
+
+    :param cache: data that is in cache
+    :type cache: list (deque())
+    :param block_num: block numbers in cache
+    :type block_num: int
+    :param k: number of sets in cache
+    :type k: int 
+    """
     header = ['#'] + [f"way {i}" for i in range(k)]
     data = [[f'set {i}'] + ["    " for _ in range(k)] for i in range(block_num)]
+    
     for i in range(len(cache)):
         for j in range(len(cache[i])):
-            data[i][j+1] = cache[i][j]
+            block_num = cache[i][j]
+            word_list = [2 * block_num + k for k in range(BLOCK_SIZE)]
+            data[i][j+1] = f"<{block_num}> {word_list}"
+
     print(85*'-')
     termtables.print(data, header=header)
 
@@ -51,6 +65,10 @@ def direct_mapping(requests: list, hexa=False) -> list:
             result.append(0)
             dmc[cache_index[i]] = requests[i]
 
+        print('request:', requests[i])
+        print('Hit' if result[i] == 1 else 'Miss')
+        draw_table(dmc, block_num, 1)
+
     hit_rate = sum(result)/len(result)
     if hexa:
         requests_hex = [hex(request) for request in requests]
@@ -74,16 +92,18 @@ def k_way_set_associative(k: int, requests: list, policy: str, hexa=False) -> li
     
     block_num = (CACHE_SIZE // BLOCK_SIZE) // k;
     kwsa = [deque(maxlen=k) for _ in range(block_num)]
-    cache_index = [request % block_num for request in requests]
+    requests_block = [request // BLOCK_SIZE for request in requests]
+    cache_index = [request % block_num for request in requests_block]
     result = []
+    draw_table(kwsa, block_num, k)
     
     if policy.upper() == 'FIFO':
         for i in range(len(requests)):
-            if requests[i] in kwsa[cache_index[i]]:
+            if requests_block[i] in kwsa[cache_index[i]]:
                 result.append(1)
             else:
                 result.append(0)
-                kwsa[cache_index[i]].append(requests[i])
+                kwsa[cache_index[i]].append(requests_block[i])
 
             print('request:', requests[i])
             print('Hit' if result[i] == 1 else 'Miss')
@@ -91,11 +111,11 @@ def k_way_set_associative(k: int, requests: list, policy: str, hexa=False) -> li
 
     elif policy.upper() == 'LRU':
         for i in range(len(requests)):
-            if requests[i] in kwsa[cache_index[i]]:
+            if requests_block[i] in kwsa[cache_index[i]]:
                 result.append(1)
             else:
                 result.append(0)
-            kwsa[cache_index[i]].append(requests[i])
+            kwsa[cache_index[i]].append(requests_block[i])
 
             print('request:', requests[i])
             print('Hit' if result[i] == 1 else 'Miss')
@@ -112,4 +132,4 @@ def k_way_set_associative(k: int, requests: list, policy: str, hexa=False) -> li
     return list(zip(requests, result)), hit_rate
 
 # print(direct_mapping(requests, hexa))
-print(k_way_set_associative(2, requests, 'LRU'))
+print(k_way_set_associative(4, requests, 'LRU'))

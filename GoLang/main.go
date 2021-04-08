bb187118759957b2d264787643bd26b43f4f8565
ali asad <aliasad059@gmail.com>
2021-04-08 09:50:26 +0430

    final commit

--- GoLang/main.go
+++ GoLang/main.go
@@ -7,23 +7,33 @@ import (
 	"strconv"
 )
 
-const cacheSize = 2048
-const blockSize = 512
+const cacheSize = 1024
+const blockSize = 256
 const wordSize = 256
-const k = 2
-const policy = "lru"
+const k = 4
+const policy = "fifo" // fifo or lru
 
 func main() {
-	var sets, wordsInBlock int
-	wordsInBlock = blockSize / wordSize
-	sets = (cacheSize / blockSize) / k
-	// asked addresses
+	wordsInBlock := blockSize / wordSize
+	sets := (cacheSize / blockSize) / k
+
+	// asked addresses in hexadecimal
 	addresses := []int{0x5, 0xC, 0xD, 0x11, 0x4, 0xC, 0xD, 0x11, 0x2, 0xD, 0x13, 0x2B, 0x3D, 0x13}
+
+	// asked addresses in decimal
 	//addresses := []int{5, 12, 13, 17, 4, 12, 13, 17, 2, 13, 19, 13, 43, 61, 19}
+
 	kWay(addresses, policy, k, sets, wordsInBlock)
 }
+
+// This function is implementation of K Way Set Associative Cache Simulation
+// policy : lru or fifo
+// k : cache's way number
+// sets : cache's set number
+// wordsInBlock : number of words in each block of cache
 func kWay(addresses []int, policy string, k, sets, wordsInBlock int) {
 	cache := makeCache(k, sets)
+	printCache(cache, sets, wordsInBlock)
 	var hits, misses int
 	for _, v := range addresses {
 		var result string
@@ -51,16 +61,18 @@ func kWay(addresses []int, policy string, k, sets, wordsInBlock int) {
 			if policy == "lru" {
 				cache[Bx] = insertAtHead(cache[Bx], v)[:k]
 			} else if policy == "fifo" {
-				cache[Bx] = append(cache[Bx], v)[0:]
+				cache[Bx] = append(cache[Bx], v)[1:]
 			}
 		}
-		printCache(cache, v, sets, wordsInBlock)
+		printCache(cache, sets, wordsInBlock)
 		fmt.Printf("request: %d\n", v*wordsInBlock)
 		fmt.Println(result)
 	}
 	fmt.Println("------------------------------------------------------")
 	fmt.Printf("Hit Rate: %f", float32(hits)/float32(misses+hits)*100)
 }
+
+// creates a 2D-array as cache
 func makeCache(k, sets int) (cache [][]int) {
 	cache = make([][]int, sets)
 	for i := 0; i < sets; i++ {
@@ -71,50 +83,35 @@ func makeCache(k, sets int) (cache [][]int) {
 	}
 	return
 }
+
+// inserts new value at head
 func insertAtHead(a []int, value int) []int {
 	a = append(a[:+1], a[:]...)
 	a[0] = value
 	return a
 }
+
+// moves the i'th element to head
 func moveToHead(a []int, i int) []int {
 	v := a[i]
 	a = append(a[:i], a[i+1:]...)
 	return insertAtHead(a, v)
 }
 
-//func printCache(cache [][]int, sets, wordsInBlock int) {
-//	for m := 0; m < sets; m++ {
-//		fmt.Printf("\n\t++++++++++++++++++++++++++++++++")
-//		fmt.Printf("\n\tSet: %d\n", m)
-//		for n := 0; n < k; n++ {
-//			fmt.Printf("\t\t----------------\n")
-//			fmt.Printf("\t\tBlock: %d\n", n)
-//			for o := 0; o < wordsInBlock; o++ {
-//				if cache[m][n] == -1 {
-//					fmt.Printf("\t\t\tW%d: -\n", o)
-//				} else {
-//					fmt.Printf("\t\t\tW%d: %d\n", o, cache[m][n]*wordsInBlock+o)
-//				}
-//			}
-//		}
-//		fmt.Printf("\t\t----------------\n")
-//
-//	}
-//	fmt.Printf("\n\t++++++++++++++++++++++++++++++++\n")
-//
-//}
-func printCache(cache [][]int, address, sets, wordsInBlock int) {
+// prints cache using go-pretty package
+func printCache(cache [][]int, sets, wordsInBlock int) {
 	fmt.Println("------------------------------------------------------")
 	t := table.NewWriter()
-	//a := []string {"a","b"}
 	t.SetStyle(table.StyleBold)
 	t.SetOutputMirror(os.Stdout)
+
 	header := make(table.Row, 0)
 	header = append(header, "#")
 	for i := 0; i < k; i++ {
 		header = append(header, "way "+strconv.Itoa(i))
 	}
 	t.AppendHeader(header)
+
 	for m := 0; m < sets; m++ {
 		row := make(table.Row, 0)
 		row = append(row, "Set "+strconv.Itoa(m))
@@ -132,7 +129,6 @@ func printCache(cache [][]int, address, sets, wordsInBlock int) {
 			}
 			row = append(row, str)
 		}
-
 		t.AppendRow(row)
 		t.AppendSeparator()
 	}
